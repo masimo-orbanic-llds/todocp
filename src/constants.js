158ac92fedd0265d21c3cfd2791717fde1ac8e42
Masimo Orbanic <masimo.orbanic@gmail.com>
2020-06-19 13:13:10 +0200

    updated

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
+import {Platform} from 'react-native';
+
 export const ASYNC_STORAGE_KEY = '@TodoCP';
-export const DEPLOYMENT_KEY = 'FX3RJRdjBHYsAjgYkLY4NwX6FiT3w8JpQZVG3';
+export const DEPLOYMENT_KEY =
+  Platform.OS === 'android' ? 'androidKeyHere' : 'iosKeyHere';
